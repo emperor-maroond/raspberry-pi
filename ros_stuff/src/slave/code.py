bfd10e5d70bd0b3d28c4685e0c84306baf50c029
emperor-maroond <erasmus.devlon@gmail.com>
2021-07-21 23:38:19 +0200

    code to control servos

--- ros_stuff/src/slave/code.py
+++ ros_stuff/src/slave/code.py
@@ -1,31 +1,48 @@
+#!/usr/bin/env python3
 import RPi.GPIO as GPIO
 import time
  
-PWM_pin = 33
+freq = 100
+PWM1_pin = 13
+PWM2_pin = 12 # GPIO 12
  
 def start_pwm():
-    global pwm
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(PWM_pin, GPIO.OUT)
-    GPIO.output(PWM_pin, GPIO.LOW)
-    pwm = GPIO.PWM(PWM_pin, 1000)
-    pwm.start(0)
+    global pwm1, pwm2
+    GPIO.setmode(GPIO.BCM)
 
-def loop():
-    while True:
-        for dc in range(0, 101, 1):
-            pwm.ChangeDutyCycle(dc)
-            time.sleep(0.01)
-        time.sleep(1)
-        for dc in range(100, -1, -1):
-            pwm.ChangeDutyCycle(dc)
-            time.sleep(0.01)
-        time.sleep(1)
+    GPIO.setup(PWM1_pin, GPIO.OUT)
+    GPIO.output(PWM1_pin, GPIO.LOW)
+    pwm1 = GPIO.PWM(PWM1_pin, freq)
+    pwm1.start(0)
+
+    GPIO.setup(PWM2_pin, GPIO.OUT)
+    GPIO.output(PWM2_pin, GPIO.LOW)
+    pwm2 = GPIO.PWM(PWM2_pin, freq)
+    pwm2.start(0)
+
+def duty_cycle(alpha, pwm, n):
+    if n == 1:
+        pw = alpha*3/400 + 0.65
+    if n == 2:
+        pw = alpha*3/400 + 0.75
+    T = 1/freq * 1000
+    dc = pw/T*100
+    pwm.ChangeDutyCycle(dc)
 
 def destroy():
-    pwm.stop()
-    GPIO.output(PWM_pin, GPIO.LOW)
+    pwm1.stop()
+    pwm2.stop()
+    GPIO.output(PWM1_pin, GPIO.LOW)
+    GPIO.output(PWM2_pin, GPIO.LOW)
     GPIO.cleanup()
      
 if  __name__ == '__main__':
     start_pwm()
+    duty_cycle(90, pwm1, 1)
+    duty_cycle(0, pwm2, 2)
+    # pwm.ChangeDutyCycle(7.5)
+    for i in range(0,3):
+        # duty_cycle(90*i)
+        time.sleep(1)
+        print(90*i)
+    destroy()
